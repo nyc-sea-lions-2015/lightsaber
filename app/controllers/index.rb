fafb7e5671e85b1656b58ec6462293d764735df5
Ben Cheng <benjamin.cheng90@gmail.com>
2015-03-17 19:26:58 -0400

    Play around with lightsaber

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,3 +3,24 @@ require 'sinatra'
 get '/' do
   'Ben Cheng'
 end
+
+get '/sealions/:ID' do
+  @id = params[:id]
+  "#{@id}"
+end
+
+get '/sealions' do
+ 'Yay, sea lions!'
+end
+
+post '/sealions' do
+  redirect '/sealions'
+end
+
+put '/sealions/:ID' do
+  redirect '/sealions'
+end
+
+delete '/sealions/:ID' do
+  redirect '/sealions'
+end
