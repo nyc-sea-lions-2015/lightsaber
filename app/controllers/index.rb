05331a5bacc7ad3a505ca42b71aded39002dd355
notryanb <notryanb@gmail.com>
2015-03-17 18:37:28 -0400

    Add modified index.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   'Lightsaber Challenge. It really works!!!'
 end
+
+post '/sealions' do
+  redirect '/'
+end
+
+put '/sealions' do
+  redirect '/'
+end
+
+delete '/sealions' do
+  redirect '/'
+end
