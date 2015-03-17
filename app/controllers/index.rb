efd18ba36fd0e941d398cdd31ef752b25b8246f8
sabron13 <kurt.schnelker@gmail.com>
2015-03-17 19:45:05 -0400

    lightsaber day 2 complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
 
 get '/' do
-  p "Kurt Schnelker"
+  "Kurt Schnelker"
+end
+
+get '/sealions' do
+  "< A list of Sealions >"
+end
+
+get '/sealions/:ID' do
+  "< A specific Sealion >"
+end
+
+post '/sealions' do
+  # "< Create a new Sealion >"
+  redirect '/'
+end
+
+put '/sealions/:ID' do
+  # "< Edit existing Sealion >"
+  redirect '/'
+end
+
+delete '/sealions/:ID' do
+  # "< Delete a specific Sealion >"
+  redirect '/'
 end
