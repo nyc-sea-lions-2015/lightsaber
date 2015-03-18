73a6a9b688233d67d0a41c150f7220f1e6de5e7e
Eveanandi Butler <butler.eveanandi@gmail.com>
2015-03-18 08:38:57 -0400

    Change placement of index file and update spec file errors

--- spec/controllers/index_spec.rb
+++ spec/controllers/index_spec.rb
@@ -5,10 +5,10 @@ describe 'Index Controller' do
   it 'Should get the / route' do
     get '/'
     expect(last_response).to be_ok
-    expect(last_response.body).to include ('Eveanandi Blossom Butler')
+    expect(last_response.body).to eq('Eveanandi Blossom Butler')
   end
 
-  it 'Should process the post /test_post route'
+  it 'Should process the post /test_post route' do
     post '/test_post'
     expect(last_response).to be_redirect
     follow_redirect!
@@ -17,7 +17,7 @@ describe 'Index Controller' do
   end
 
   it 'Should process the put/test_put route' do
-    put '/test_post'
+    put '/test_put'
     expect(last_response).to be_redirect
     follow_redirect!
     expect(last_response).to be_ok
@@ -28,7 +28,7 @@ describe 'Index Controller' do
     delete '/test_delete'
     expect(last_response).to be_redirect
     follow_redirect!
-    expect(last_response.status).to be_ok
+    expect(last_response).to be_ok
     expect(last_response.body).to include('Eveanandi Blossom Butler')
   end
 
