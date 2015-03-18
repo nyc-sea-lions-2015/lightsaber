d5f5f3e9ea6e829f4424df1760ff34e32c93c1f4
Awesome Ninja Fighter <samguergen@gmail.com>
2015-03-17 21:29:02 -0400

    added routes and spec tests for routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,30 @@
 require 'sinatra'
 
 get '/' do
-
 'Samantha'
 
+end
+
+get '/sealions'
+'The best'
+end
+
+
+post '/post_something' do
+  params
+  puts 'Hi Sam, you posted something'
+  redirect '/sealions'
+end
+
+put '/update_something' do
+  params
+  puts 'Hi Sam, you just updated something'
+  redirect '/sealions'
+end
+
+
+delete '/delete_something' do
+  params
+  puts 'Hi Sam, you just deleted something'
+  redirect '/sealions'
 end
