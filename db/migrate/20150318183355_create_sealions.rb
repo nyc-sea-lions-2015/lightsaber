df797d613bc01e10607aa2133b4d68d476ebef80
Ben Cheng <benjamin.cheng90@gmail.com>
2015-03-18 19:19:05 -0400

    Add more seeds

--- db/migrate/20150318183355_create_sealions.rb
+++ db/migrate/20150318183355_create_sealions.rb
@@ -1,6 +1,6 @@
 class CreateSealions < ActiveRecord::Migration
   def change
-    create_table :sealion do |t|
+    create_table :sealions do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
 
