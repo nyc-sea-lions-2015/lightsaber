f0129fe3f74c49ec92d8aed19b48586ae33e0aa2
Angel Baek <angelbaek1@gmail.com>
2015-03-18 20:58:58 -0400

    R of CRUD

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,32 +7,41 @@ get '/sealions' do
   "...Lions! Tigers! Bears! Oh My!"
 end
 
-get '/sealions/:ID' do
-  "This id # is 9"
-end
+# get '/sealions/:ID' do
+#   "This id # is 9"
+# end
 
-post '/post_a_new_post' do
-  redirect '/sealions'
-end
+# post '/post_a_new_post' do
+#   redirect '/sealions'
+# end
 
-put '/putting_on_the_ritz' do
-  redirect '/sealions'
-end
+# put '/putting_on_the_ritz' do
+#   redirect '/sealions'
+# end
 
-delete '/delete_me' do
-  redirect '/sealions'
-end
+# delete '/delete_me' do
+#   redirect '/sealions'
+# end
 
 #create
-post '/sealion/:name' do
-  new_sealion = Sealion.create(params[:name])
-  redirect '/sealions'
+# post '/sealion/:name/:weight' do
+#   new_sealion = Sealion.create({params[:name], params[:weight]})
+#   redirect '/sealions'
+# end
+
+#read
+get '/sealions/:name' do
+  # Sealion.find_by(name: params[:name]).name
+  "Hello #{params[:name].capitalize}"
 end
 
+
 #update
-put '/sealion/:name' do
-  charlie = Sealion.find_by(params[:name])
-  charlie.update_attribute(name: "Charlotte")
-  redirect '/sealions'
-end
+# put '/sealion/:name' do
+#   charlie = Sealion.find_by(params[:name])
+#   charlie.update_attribute(name: "Charlotte")
+#   redirect '/sealions'
+# end
+
+#delete
 
