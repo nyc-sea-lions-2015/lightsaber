b0bca6d86d35d5e83d5e94b736ebebfbd73ea8fb
Ben Cheng <benjamin.cheng90@gmail.com>
2015-03-18 20:53:39 -0400

    Edit spec

--- spec/controllers/index_spec.rb
+++ spec/controllers/index_spec.rb
@@ -5,36 +5,37 @@ describe 'IndexController' do
   it 'Should get the /sealions route' do
       get '/sealions'
       expect(last_response).to be_ok
-      expect(last_response.body).to include('Yay, sea lions!')
+      expect(last_response.body).to include('Tracy Teague')
   end
 
-  it 'Should get the /sealions/:ID route' do
-      get '/sealions/:ID?id=3'
-      expect(last_response).to be_ok
-      expect(last_response.body).to include("#{@id}")
-  end
+  # it 'Should get the /sealions/:ID route' do
+  #     get '/sealions/:ID?id=3'
+  #     expect(last_response).to be_ok
+  #     expect(last_response.body).to include("#{@id}")
+  # end
 
-  it 'Should post a new route' do
-      post '/sealions'
-      expect(last_response).to be_redirect
-      follow_redirect!
-      expect(last_response).to be_ok
-      expect(last_response.body).to include('Yay, sea lions!')
-  end
+  # it 'Should post a new route' do
+  #     post '/sealions'
+  #     expect(last_response).to be_redirect
+  #     follow_redirect!
+  #     expect(last_response).to be_ok
+  #     expect(response).to be_instance_of?(Sealion)
+  #     expect(last_response.body).to include('Yay, sea lions!')
+  # end
 
-  it 'Should update an existing route' do
-      put '/sealions/:ID', :params => {id: 4}
-      expect(last_response).to be_redirect
-      follow_redirect!
-      expect(last_response).to be_ok
-      expect(last_response.body).to include('Yay, sea lions!')
-  end
+  # it 'Should update an existing route' do
+  #     put '/sealions/:ID', :params => {id: 4}
+  #     expect(last_response).to be_redirect
+  #     follow_redirect!
+  #     expect(last_response).to be_ok
+  #     expect(last_response.body).to include('Yay, sea lions!')
+  # end
 
-  it 'Should delete existing route' do
-      delete '/sealions/:ID'
-      expect(last_response).to be_redirect
-      follow_redirect!
-      expect(last_response).to be_ok
-      expect(last_response.body).to include('Yay, sea lions!')
-  end
+  # it 'Should delete existing route' do
+  #     delete '/sealions/:ID'
+  #     expect(last_response).to be_redirect
+  #     follow_redirect!
+  #     expect(last_response).to be_ok
+  #     expect(last_response.body).to include('Yay, sea lions!')
+  # end
 end
