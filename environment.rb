42f944bee8bcec6cd1f90fc3ee8a767c07cb218b
Lauren Nicole Roth <laurennicoleroth@gmail.com>
2015-03-19 22:07:47 -0400

    Restore Rakefile

--- environment.rb
+++ environment.rb
@@ -1,4 +1,4 @@
-require 'erb'
+require 'active_record'
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
 
@@ -14,11 +14,10 @@ require 'pathname'
 APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
 
+#Load Models
+Dir[APP_ROOT.join('app', 'models', '*.rb')].each { |file| require file }
+
 #Load Controllers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 
-
-configure do
- set :root, APP_ROOT.to_path
- set :views, File.join(APP_ROOT,"app","views")
-end
+require APP_ROOT.join('database')
