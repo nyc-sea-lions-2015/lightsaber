93818e960b0bef9a68f59d054b30ab352225dc2a
Notika314 <notika314@gmail.com>
2015-03-19 20:04:08 -0400

    add changes to environment file

--- environment.rb
+++ environment.rb
@@ -9,6 +9,8 @@ require "active_record"
 
 require 'uri'
 require 'pathname'
+require 'erb'
+
 
 
  APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
@@ -17,5 +19,10 @@ require 'pathname'
  Dir[APP_ROOT.join('app', 'models', '*.rb')].each { |file| require file }
  #Load Controllers
  Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+
+ configure do
+ set :root, APP_ROOT.to_path
+ set :views, File.join(APP_ROOT,"app","views")
+end
 
  require APP_ROOT.join('database')
