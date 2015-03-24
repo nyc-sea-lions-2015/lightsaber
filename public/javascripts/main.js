84cc16d8d3eeed399c100af487b539e4d7dd6154
Dan Asselin <dasselin86@gmail.com>
2015-03-24 13:17:59 -0400

    Write Jasmine test for subtract function and code to pass test in main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,11 @@
 var calculator = {
+
   add: function(a, b){
     return a + b;
+  },
+
+  subtract: function(a, b){
+    return a - b;
   }
+
 };
--- spec/javascripts/javascript_spec.js
+++ spec/javascripts/javascript_spec.js
@@ -6,4 +6,9 @@ describe ('Javascript Calculator', function(){
   it ('Should add two variables', function(){
     expect(calculator.add(1, 2)).toEqual(3);
   });
+
+  it ('Should subtract two variables', function(){
+    expect(calculator.subtract(3, 1)).toEqual(2);
+  });
+
 });
