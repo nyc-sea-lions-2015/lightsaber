9a15e508fbc7d7f8684332eeec5c6e21f653e48e
Lauren Nicole Roth <laurennicoleroth@gmail.com>
2015-03-23 21:46:51 -0400

    Add specs for a and b in object literal for later use. 	modified:   spec/javascripts/javascript_spec.js

--- spec/javascripts/javascript_spec.js
+++ spec/javascripts/javascript_spec.js
@@ -3,3 +3,17 @@ describe("Canary test", function() {
     expect(true).toBe(true);
   });
 });
+
+var myObj = {a: 7, b: 4}
+
+describe("Obj test", function() {
+  it("should have a number 7", function(){
+  	expect(myObj.a).toBe(7);
+  });
+});
+
+describe("Obj test", function() {
+  it("should have a number 4", function(){
+  	expect(myObj.b).toBe(4);
+  });
+});
