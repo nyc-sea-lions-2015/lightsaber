a71b80fc533f2b1117c8fa001458ac5ed0e111bc
Angel Baek <angelbaek1@gmail.com>
2015-03-23 20:20:35 -0400

    jasmine js tests run

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,87 @@
+#read
 
 get '/' do
-  "Helllloo there!"
+  erb :"index"
 end
 
 get '/sealions' do
-  "...Lions! Tigers! Bears! Oh My!"
+  erb :"sealions/index"
 end
+
+# get '/sealions/:name' do
+#   name = Sealion.find_by(name: params[:name])
+#   unless name.nil?
+#     "This is #{Sealion.find(params[:name]}"
+#   else
+#     [404, "Sealion does not exist FOOL!"]
+#   end
+# end
+
+post '/sealions/new' do
+
+  Sealion.create(params)
+  redirect '/sealions'
+end
+
+get '/sealions/new' do
+  erb(:new, locals: {name: params[:name], weight: params[:weight]})
+end
+
+
+get '/sealions/:id' do
+  friend = Sealion.find_by(id: params[:id])
+  unless friend.nil?
+    "This is #{Sealion.find(params[:id]).name}. My number is #{params[:id]}"
+  else
+    [404, "Sealion does not exist FOOL!"]
+  end
+end
+
+
+
+# get '/sealions/:name' do
+#   name = Sealion.find_by(name: params[:name])
+#   unless name.nil?
+#     "This is #{Sealion.find(params[:name]}"
+#   else
+#     [404, "Sealion does not exist FOOL!"]
+#   end
+# end
+
+
+#create
+
+
+#update
+put '/sealions/:id/update' do
+  charlie = Sealion.find_by(id: params[:id])
+  charlie.update_attribute(name: "Charlotte")
+  redirect '/sealions'
+end
+
+delete '/sealions/delete' do
+  redirect '/sealions'
+end
+
+
+
+
+# get '/sealions/:name' do
+#   new_sealion = Sealion.find_by(params[:name])
+#   "Hello #{new_sealion.name.capitalize}"
+
+# end
+
+# # create
+# post '/sealion/:name' do
+#   new_sealion = Sealion.create(params)
+#   redirect '/sealions'
+# end
+
+
+
+
+#delete
 
 # get '/sealions/:ID' do
 #   "This id # is 9"
@@ -18,30 +94,3 @@ end
 # put '/putting_on_the_ritz' do
 #   redirect '/sealions'
 # end
-
-# delete '/delete_me' do
-#   redirect '/sealions'
-# end
-
-#create
-# post '/sealion/:name/:weight' do
-#   new_sealion = Sealion.create({params[:name], params[:weight]})
-#   redirect '/sealions'
-# end
-
-#read
-get '/sealions/:name' do
-  # Sealion.find_by(name: params[:name]).name
-  "Hello #{params[:name].capitalize}"
-end
-
-
-#update
-# put '/sealion/:name' do
-#   charlie = Sealion.find_by(params[:name])
-#   charlie.update_attribute(name: "Charlotte")
-#   redirect '/sealions'
-# end
-
-#delete
-
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,6 +1,8 @@
 require_relative 'spec_helper'
-
+#-----get-----
 describe 'Sea Lions Controller' do
+# let(:charlie) {Sealion.create(name: "Charlie", weight: 234)}
+
   it 'should get the / route' do
     get '/'
     expect(last_response).to be_ok
@@ -13,33 +15,55 @@ describe 'Sea Lions Controller' do
     expect(last_response.body).to include("...Lions! Tigers! Bears! Oh My!")
   end
 
-  it 'should get the /sealions/:ID route where the id = is 6' do
-    get '/sealions/:ID?id=9'
+  it 'should get the specific sealion by id #' do
+    get "/sealions/:ID?id=1"
     expect(last_response).to be_ok
-    expect(last_response.body).to include( "This id # is 9")
+    # expect(last_response.body).to include("This is sealion #1")
+    expect(last_response.body).to include("His name is Charlie")
   end
 
-  it 'should redirect to /sealions route' do
-    post '/post_a_new_post'
+  it "should change Charlie's name to Charlotte " do
+    put "sealions/#{charlie.name}/update"
     expect(last_response).to be_redirect
     follow_redirect!
-    expect(last_response.body).to include("...Lions! Tigers! Bears! Oh My!")
+    expect(last_response.body).to include("Charlotte")
   end
 
-  it 'should redirect to /sealions route' do
-    put '/putting_on_the_ritz'
-    expect(last_response).to be_redirect
-    follow_redirect!
-    expect(last_response.body).to include("...Lions! Tigers! Bears! Oh My!")
-  end
+  it "should create"
 
-  it 'should redirect to /sealions' do
-    delete '/delete_me'
-    expect(last_response).to be_redirect
-    follow_redirect!
-    expect(last_response.body).to include("...Lions! Tigers! Bears! Oh My!")
+  # it 'should get the specific sealion with the name Charlie' do
+  #   get '/sealions/Charlie'
+  #   expect(last_response).to be_ok
+  #   expect(last_response.body).to include( "Hello Charlie")
+  # end
+  # it 'should redirect to /sealions route' do
+  #   post '/post_a_new_post'
+  #   expect(last_response).to be_redirect
+  #   follow_redirect!
+  #   expect(last_response.body).to include("...Lions! Tigers! Bears! Oh My!")
+  # end
+#------post-------
 
-  end
+
+
+
+
+
+
+  # it 'should redirect to /sealions route' do
+  #   put '/putting_on_the_ritz'
+  #   expect(last_response).to be_redirect
+  #   follow_redirect!
+  #   expect(last_response.body).to include("...Lions! Tigers! Bears! Oh My!")
+  # end
+
+  # it 'should redirect to /sealions' do
+  #   delete '/delete_me'
+  #   expect(last_response).to be_redirect
+  #   follow_redirect!
+  #   expect(last_response.body).to include("...Lions! Tigers! Bears! Oh My!")
+
+  # end
 
 
 end
