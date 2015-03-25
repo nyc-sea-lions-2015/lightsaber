95bb7544d57f48d9d9a64b74ed0cf90ae230c44e
Ben Cheng <benjamin.cheng90@gmail.com>
2015-03-25 08:23:04 -0400

    Fix error buttons to display

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,34 +1,27 @@
-var calculator = {
-add: function(x, y) {
-  return (x + y);
-  },
-subtract: function(x, y) {
-  return (x - y);
-  },
-multiply: function(x, y){
-  return (x * y);
-  },
-divide: function(x, y) {
-  return (x / y);
-  },
-};
+// var calculator = {
+// add: function(x, y) {
+//   return (x + y);
+//   },
+// subtract: function(x, y) {
+//   return (x - y);
+//   },
+// multiply: function(x, y){
+//   return (x * y);
+//   },
+// divide: function(x, y) {
+//   return (x / y);
+//   },
+// };
 
 var errorModule = function() {
-  var hide = document.getElementsByClassName("hide");
+  var show_error = document.getElementById("error");
+  var hide_error = document.getElementById("fixer");
 
-  return {
-    hide: function() {
-      hide.style.visibility='hidden';
-    },
-    show: function() {
-      hide.style.visibility='visible';
-    },
-    on: function(onClick, ahhh) {
-      hide.addEventListener(onClick, ahhh);
-    },
-    trigger: function(event) {
-      var build = new Event(event);
-      hide.dispatchEvent(build);
-    }
-  }
+    show_error.addEventListener('click', function(){
+      var error_button = document.getElementById("error_msg");
+      error_button.style.visibility="visible"});
+
+    hide_error.addEventListener('click', function(){
+      var error_button = document.getElementById("error_msg");
+      error_button.style.visibility="hidden"});
 };
